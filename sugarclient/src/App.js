ab414b30e6ef6e64bd39325e6c6d814ebb2b30a5
John Ochs <john.w.ochs0@gmail.com>
2019-12-04 14:53:02 -0600

    added basic react layout components

--- sugarclient/src/App.js
+++ sugarclient/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Wrapper from "./components/Wrapper";
+import Nav from "./components/Nav";
+import Action from "./components/Action"
+import Footer from "./components/Footer";
 import logo from './logo.svg';
 import './App.css';
 import {
@@ -13,13 +16,29 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            things: 0
+            whatever: null
         }
     };
     render() {
         return (
             <Wrapper>
-
+                <div className="container">
+                    <div className="row">
+                        <h1 className="header left black-text">facePalm</h1>
+                    </div>
+                    <div className="row">
+                        <Router>
+                            <Nav />
+                            <Switch>
+                                {/* <Route path="/" exact component={} /> */}
+                            </Switch>
+                        </Router>
+                    </div>
+                    <div className="row">
+                        <Action />
+                    </div>
+                </div>
+                <Footer />
             </Wrapper>
         );
     }
