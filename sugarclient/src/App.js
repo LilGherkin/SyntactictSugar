1404bfb2a3b5e7c47073f56cb0d554142f17a189
John Ochs <john.w.ochs0@gmail.com>
2019-12-04 13:42:37 -0600

    not sure what was added but I need to push to pull

--- sugarclient/src/App.js
+++ sugarclient/src/App.js
@@ -3,25 +3,25 @@ import Wrapper from "./components/Wrapper";
 import logo from './logo.svg';
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      things: 0
-    }
-  };
-  render() {
-    return (
-      <Wrapper>
+    constructor(props) {
+        super(props);
+        this.state = {
+            things: 0
+        }
+    };
+    render() {
+        return (
+            <Wrapper>
 
-      </Wrapper>
-    );
-  }
+            </Wrapper>
+        );
+    }
 }
 export default App;
