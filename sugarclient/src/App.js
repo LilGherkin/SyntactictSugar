20c74ac2d6845dc47224cb115094c42e14eb9636
John Ochs <john.w.ochs0@gmail.com>
2019-12-05 18:16:08 -0600

    added minor react things

--- sugarclient/src/App.js
+++ sugarclient/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Wrapper from "./components/Wrapper";
 import Nav from "./components/Nav";
-import Action from "./components/Action"
+// import Action from "./components/Action";
 import Footer from "./components/Footer";
-import logo from './logo.svg';
+import Home from "./components/Home";
 import './App.css';
 import {
     BrowserRouter as Router,
@@ -26,21 +26,17 @@ class App extends React.Component {
                     <div className="row">
                         <h1 className="header left black-text">facePalm</h1>
                     </div>
-                    <div className="row">
-                        <Router>
-                            <Nav />
-                            <Switch>
-                                {/* <Route path="/" exact component={} /> */}
-                            </Switch>
-                        </Router>
-                    </div>
-                    <div className="row">
-                        <Action />
-                    </div>
+                    <Router>
+                        <Nav />
+                        <Switch>
+                            <Route path="/" exact component={Home} />
+                        </Switch>
+                    </Router>
                 </div>
                 <Footer />
             </Wrapper>
         );
-    }
-}
+    };
+};
+
 export default App;
