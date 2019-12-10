e63759d73d33b93efde94edfc5f6d2a661f5279d
John Ochs <john.w.ochs0@gmail.com>
2019-12-10 15:18:40 -0600

    yes

--- sugarclient/src/components/Userpost/index.js
+++ sugarclient/src/components/Userpost/index.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { useAuth0 } from "../../react-auth0-spa";
 
 function Userpost(props) {
     return (
         <>
             <li class="collection-item avatar">
-                <img src="images/yuna.jpg" alt="" class="circle" />
+                <img src="?" alt="" class="circle" />
                 <span class="title">Title</span>
-                <p>First Line
-                    Second Line
-                </p>
+                <p>First Line</p>
                 <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
             </li>
         </>
